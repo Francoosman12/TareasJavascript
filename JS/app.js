496289ad9b0acf51782b30ca2f975b4b84d4e9ca
francoosman12 <francoosman12@gmail.com>
2022-03-22 11:12:02 -0300

    La tarea del 23/03 ya fue subida

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+//Mensaje de bienvenida
+alert('Bienvenidos a mi página web');
+
+//Programa en la pantalla
+document.write('Hello World');
+
+
+//Suma
+var numero1 = parseInt(prompt('Ingrese el primer número'));
+let numero2 = parseInt(prompt('Ingrese el segundo número'));
+
+let resultado = alert('El resultado es: ' + (numero1 + numero2));
+
+
+
